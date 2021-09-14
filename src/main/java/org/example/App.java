9e504bda3fd901dc03f5df16cea2ccf6f835ca56
Reese2as <89660247+Reese2as@users.noreply.github.com>
2021-09-13 22:46:52 -0400

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,13 +10,17 @@ public class App {
         Scanner user_input = new Scanner(System.in);
         int L, W, A_feet;
         final float Conversion = 0.09290304f;
+        
         System.out.print("What is the length of the room in feet? ");
         L = user_input.nextInt();
         System.out.print("What is the width of the room in feet? ");
         W = user_input.nextInt();
+        
         System.out.println(String.format("You entered dimensions of %d feet by %d feet", L, W));
+        
         A_feet=(L*W);
         float A_meters = (float)(A_feet*Conversion);
+        
         System.out.println(String.format("The area is\n%d square feet\n%.3f square meters",A_feet,A_meters));
     }
 }
